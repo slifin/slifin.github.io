b320d8db46cb3011c2a9c307eae6dbd463cdbcc4
Adrian Smith <arcanine@Adrians-MacBook-Air.local>
2014-03-04 18:12:42 +0000

    fix semi-colon

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var main_state = {
 
     create: function() {
       this.horse = this.game.add.sprite(20 ,20,'horse');
-      this.horse.body.gravity.y = 1000
+      this.horse.body.gravity.y = 1000;
 
     },
 
